8f8f2c7d04793b43188bc91da022f706325156e8
Doranni <doranna5@gmail.com>
2022-06-05 20:52:12 +0300

    Fix movement, jump animation.

--- IV_Inside the Labyrinth/Assets/Scripts/AnimationAndMovementController.cs
+++ IV_Inside the Labyrinth/Assets/Scripts/AnimationAndMovementController.cs
@@ -4,129 +4,208 @@ using UnityEngine;
 
 public class AnimationAndMovementController : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float jumpingHight;
-    [SerializeField] private float jumpingAsideForce;
-    [SerializeField] private float addGravity;
+    [SerializeField] private float movementSpeed, rotationSpeed;
+    [SerializeField] private float jumpUpForce, jumpAsideForce;
+    [SerializeField] private LayerMask floorLM;
 
     private CharacterController characterController;
     private Animator animator;
+    private GameInput gameInput;
 
-    private float rotationInput;
-    private float verticalInput;
-    private bool isOnDash = false;
+    private float rotationInput, verticalInput;
+    private bool accelerationInput;
+
+    private Vector3 moveDirection, jumpDirection;
+
     private float acceleration = 1f;
     private float maxAcceleration = 2f;
-    private float gravity;
-    private Vector3 moveDirection;
+
+    private bool isMovementLocked = false;
+    private bool isFalling = false;
+    private bool isJumpingUp = false;
+
+    int animHashVelocity, animHashJump, animHashFalling, animHashLanding;
+
+    private void Awake()
+    {
+        gameInput = new GameInput();
+
+        gameInput.Player.Move.started += context =>
+        {
+            verticalInput = context.ReadValue<float>();
+        };
+        gameInput.Player.Move.performed += context =>
+        {
+            verticalInput = context.ReadValue<float>();
+        };
+        gameInput.Player.Move.canceled += context =>
+        {
+            verticalInput = context.ReadValue<float>();
+        };
+
+        gameInput.Player.Rotate.started += context =>
+        {
+            rotationInput = context.ReadValue<float>();
+        };
+        gameInput.Player.Rotate.performed += context =>
+        {
+            rotationInput = context.ReadValue<float>();
+        };
+        gameInput.Player.Rotate.canceled += context =>
+        {
+            rotationInput = context.ReadValue<float>();
+        };
+
+        gameInput.Player.Acceleration.started += context =>
+        {
+            accelerationInput = context.ReadValueAsButton();
+        };
+        gameInput.Player.Acceleration.canceled += context =>
+        {
+            accelerationInput = context.ReadValueAsButton();
+        };
+
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        animHashVelocity = Animator.StringToHash("Velocity");
+        animHashJump = Animator.StringToHash("Jump");
+        animHashFalling = Animator.StringToHash("Falling");
+        animHashLanding = Animator.StringToHash("Landing");
+    }
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-        gravity = Physics.gravity.y;
+        characterController.Move(Vector3.down);
     }
 
     void Update()
     {
-        rotationInput = Input.GetAxis("Mouse X");
-        verticalInput = Input.GetAxis("Vertical");
+        if (isFalling)
+        {
+            if (characterController.isGrounded)
+            {
+                isFalling = false;
+                animator.SetTrigger(animHashLanding);
+            }
+        }
+        else if (isJumpingUp)
+        {
+            characterController.Move(jumpDirection * Time.deltaTime);
+        }
+        else if (!isMovementLocked)
+        {
+            if (!characterController.isGrounded)
+            {
+                Vector3 p1 = transform.position + characterController.center
+                    - new Vector3(0, characterController.height * 0.5f - 0.5f, 0);
+                Vector3 p2 = p1 + new Vector3(0, characterController.height - 1, 0);
+                if (!Physics.CapsuleCast(p1, p2, characterController.radius,
+                    Vector3.down, 2f, floorLM))
+                {
+                    isFalling = true;
+                    isMovementLocked = true;
+                    animator.SetTrigger(animHashFalling);
+                }
+            }
+            else
+            {
+                HandleRotation();
+                SetAcceleration();
+                HandleMovement();
+            }
+        }
 
-        HandleRotation();
-        SetAcceleration();
-        HandleAnimation();
+        // Debug
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Debug.Log("Player: isGrounded - " + characterController.isGrounded);
+        }
     }
 
-    private void FixedUpdate()
-    {
-        SetMoveDirection();
-        characterController.Move(moveDirection * Time.fixedDeltaTime);
-    }
 
     private void HandleRotation()
     {
         if (characterController.isGrounded)
         {
-            transform.Rotate(Vector3.up * rotationInput * rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up, Mathf.Lerp(0, rotationInput * rotationSpeed,
+                Time.deltaTime), Space.Self);
         }
     }
 
-    private void SetMoveDirection()
+    private void HandleMovement()
     {
-        if (characterController.isGrounded)
+        if (gameInput.Player.JumpUp.WasPressedThisFrame())
+        {
+            isMovementLocked = true;
+            jumpDirection = transform.forward * verticalInput * movementSpeed
+                * acceleration + Vector3.up * jumpUpForce;
+            animator.SetTrigger(animHashJump);
+        }
+        // Jumping left
+        else if (gameInput.Player.JumpLeft.WasPressedThisFrame())
+        {
+
+        }
+        // Jumping right
+        else if (gameInput.Player.JumpRight.WasPressedThisFrame())
+        {
+
+        }
+        else
         {
             moveDirection = transform.forward * verticalInput * movementSpeed * acceleration;
-
-            // Jumping 
-            if (Input.GetButtonDown("Jump"))
-            {
-                moveDirection.y = jumpingHight;
-            }
-            // Jumping left
-            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                moveDirection = moveDirection.normalized + transform.right * -1;
-                moveDirection *= jumpingAsideForce;
-                moveDirection.y = jumpingHight;
-            }
-            // Jumping right
-            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                moveDirection = moveDirection.normalized + transform.right;
-                moveDirection *= jumpingAsideForce;
-                moveDirection.y = jumpingHight;
-            }
-            else
-            {
-                moveDirection.y = gravity;
-            }
-        }
-        // Falling
-        else
-        {
-            moveDirection.y += addGravity * Time.fixedDeltaTime;
-        }
-    }
-
-    private void HandleAnimation()
-    {
-        if (characterController.isGrounded)
-        {
-            animator.SetFloat("Velocity", verticalInput * acceleration);
-        }
-        else
-        {
-            animator.SetFloat("Velocity", 0);
+            characterController.SimpleMove(moveDirection);
+            animator.SetFloat(animHashVelocity, verticalInput * acceleration);
         }
     }
 
     private void SetAcceleration()
     {
         // Dashing
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
-        {
-            isOnDash = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
-        {
-            isOnDash = false;
-        }
-        if (isOnDash && characterController.isGrounded && acceleration < maxAcceleration)
+        if (accelerationInput && characterController.isGrounded && acceleration < maxAcceleration)
         {
             acceleration += Time.deltaTime;
         }
-        else if (!isOnDash && characterController.isGrounded && acceleration > 1f)
+        else if (!accelerationInput && characterController.isGrounded && acceleration > 1f)
         {
             acceleration -= Time.deltaTime;
         }
-        else if (isOnDash && acceleration > maxAcceleration)
+        else if (accelerationInput && acceleration > maxAcceleration)
         {
             acceleration = maxAcceleration;
         }
-        else if (!isOnDash && acceleration < 1f)
+        else if (!accelerationInput && acceleration < 1f)
         {
             acceleration = 1f;
         }
+    }
+
+    public void JumpUpStart()
+    {
+        isJumpingUp = true;
+        Debug.Log($"Player is Jumping");
+    }
+
+    public void JumpUpFinish()
+    {
+        isJumpingUp = false;
+        isFalling = true;
+        Debug.Log($"Player is Jumping: velocity - {animator.GetFloat(animHashVelocity)}, high - {transform.position.y}");
+    }
+
+    public void UnlockMovement()
+    {
+        isMovementLocked = false;
+    }
+
+    private void OnEnable()
+    {
+        gameInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        gameInput.Disable();
     }
 }
